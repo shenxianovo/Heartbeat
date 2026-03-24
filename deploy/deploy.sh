b67f8b590e02ea9c2c620320fe3e8103239578dd
shenxianovo <shenxianovo@outlook.com>
2026-03-24 20:08:47 +0800

    deploy: use urls parameter to set port

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -37,7 +37,7 @@ stop_backend() {
 start_backend() {
     echo "Starting backend..."
     nohup dotnet "$PUBLISH_DIR/Heartbeat.Server.dll" \
-        --environment $DOTNET_ENV \
+        --urls "http://0.0.0.0:5023" \
         > "$LOG_FILE" 2>&1 &
     echo $! > "$PID_FILE"
     echo "Started (PID $!)"
